40b4271c7e7a83aa706719f475ee7b3fff4d7927
alpatyk <you@example.comalbertopatyk@gmail.com>
2024-09-16 10:09:12 -0300

    adicionando a limitação de atributo para maximo 15

--- src/main/kotlin/atributos/DistribuicaoPontosStrategy.kt
+++ src/main/kotlin/atributos/DistribuicaoPontosStrategy.kt
@@ -11,21 +11,32 @@ class DistribuicaoPontosStrategy : atributoStrategy {
                 println("$atributo (atual: $valor): Quantos pontos deseja adicionar?")
                 val pontosAdicionar = readLine()?.toIntOrNull() ?: 0
 
-                if (pontosAdicionar <= pontosRestantes) {
-                    val novoValor = atributos[atributo]!! + pontosAdicionar
-                    if (novoValor > 15) {
+                val novoValor = atributos[atributo]!! + pontosAdicionar
+                when {
+                    novoValor > 15 -> {
+                        println("O valor máximo para qualquer atributo é 15.")
+                    }
+                    novoValor == 15 -> {
+                        // Adicionar o 15º ponto custa 2 pontos para cada 1 adicionado
                         if (pontosAdicionar * 2 <= pontosRestantes) {
                             atributos[atributo] = novoValor
                             pontosRestantes -= pontosAdicionar * 2
                         } else {
-                            println("Não há pontos suficientes para adicionar ao atributo $atributo.")
+                            println("Não há pontos suficientes para aumentar o atributo $atributo para 15.")
                         }
-                    } else {
-                        atributos[atributo] = novoValor
-                        pontosRestantes -= pontosAdicionar
                     }
-                } else {
-                    println("Você não tem pontos suficientes para isso.")
+                    novoValor in 9..14 -> {
+                        // Aumentos normais para atributos até 14
+                        if (pontosAdicionar <= pontosRestantes) {
+                            atributos[atributo] = novoValor
+                            pontosRestantes -= pontosAdicionar
+                        } else {
+                            println("Você não tem pontos suficientes para adicionar ao atributo $atributo.")
+                        }
+                    }
+                    else -> {
+                        println("Entrada inválida. Certifique-se de adicionar um número de pontos válido.")
+                    }
                 }
             }
         }
